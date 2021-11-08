30f67c398317a551a1a01ee55e6b81175f90a4c9
chandhanapriya T <92292189+chandhanapriya@users.noreply.github.com>
2021-11-08 18:45:37 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+
+namespace Demo_Reflection
+{
+   class Program
+    {
+      static void Main(string[] args)
+        {
+            Console.WriteLine("Demo reflection in C#");
+            Type T = typeof(int);
+            Console.WriteLine("Type of name:{0}", T.Name);
+            Console.WriteLine("Full name of Typedefined\t :{0}", T.FullName);
+            Console.WriteLine("Type of assembly defined",T.Assembly);
+            Console.WriteLine("Imaformation of base type\t {0}", T.BaseType);
+
+            //creating an object
+            Console.WriteLine("creating dynamic objects");
+            Dynamic_Object O= new Dynamic_Object();
+            O.firstmethod();
+            O.Secondmethod();
+            Dynamic_Object O1= new Dynamic_Object();
+            O1.Secondmethod();
+            O1.firstmethod();
+           
+
+
+        }
+    }
+}
---
+++ reflectionclass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Demo_Reflection
+{
+    class Dynamic_Object
+    {
+    public Dynamic_Object() { }
+    public Dynamic_Object(string Msg) { }
+
+    public void firstmethod()
+    {
+        Console.WriteLine("Inside method 1");
+    }
+    public void Secondmethod()
+    {
+        Console.WriteLine("inside method 2");
+    }
+
+
+}
+}
